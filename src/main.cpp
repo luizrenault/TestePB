2cb80c5184c18eacf24656dd42f6acdce61a0a73
RoboIME Lab <roboime@googlegroups.com>
2016-09-27 13:04:14 -0300

    Exemplo funcional para decodificação pacote < 32 bytes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 /**
-*****************************************************************************
-**
-**  File        : main.c
-**
-**  Abstract    : main function.
-**
-**  Functions   : main
-**
-**  Environment : Atollic TrueSTUDIO(R)
-**                STMicroelectronics STM32F4xx Standard Peripherals Library
-**
-**  Distribution: The file is distributed "as is", without any warranty
-**                of any kind.
-**
-**  (c)Copyright Atollic AB.
-**  You may use this file as-is or modify it according to the needs of your
-**  project. This file may only be built (assembled or compiled and linked)
-**  using the Atollic TrueSTUDIO(R) product. The use of this file together
-**  with other tools than Atollic TrueSTUDIO(R) is not permitted.
-**
-*****************************************************************************
-*/
+ *****************************************************************************
+ **
+ **  File        : main.c
+ **
+ **  Abstract    : main function.
+ **
+ **  Functions   : main
+ **
+ **  Environment : Atollic TrueSTUDIO(R)
+ **                STMicroelectronics STM32F4xx Standard Peripherals Library
+ **
+ **  Distribution: The file is distributed "as is", without any warranty
+ **                of any kind.
+ **
+ **  (c)Copyright Atollic AB.
+ **  You may use this file as-is or modify it according to the needs of your
+ **  project. This file may only be built (assembled or compiled and linked)
+ **  using the Atollic TrueSTUDIO(R) product. The use of this file together
+ **  with other tools than Atollic TrueSTUDIO(R) is not permitted.
+ **
+ *****************************************************************************
+ */
 
 /* Includes */
 #include "stm32f4xx.h"
@@ -35,104 +35,57 @@
 /* Private functions */
 
 /**
-**===========================================================================
-**
-**  Abstract: main program
-**
-**===========================================================================
-*/
+ **===========================================================================
+ **
+ **  Abstract: main program
+ **
+ **===========================================================================
+ */
 
-bool robot_commands_encode_callback(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
-{
-	int i;
-	grSim_Robot_Command robotcommand={};
 
-	for(i=1; i<6;i++){
-		robotcommand.id=i;
-		robotcommand.veltangent=0.0f;
-		robotcommand.velnormal=0.0f;
-		robotcommand.velangular=0.0f;
-		robotcommand.kickspeedx=0.0f;
-		robotcommand.kickspeedz=0.0f;
-		robotcommand.spinner=false;
-		robotcommand.wheelsspeed=false;
-		robotcommand.has_wheel1=false;
-		robotcommand.has_wheel2=false;
-		robotcommand.has_wheel3=false;
-		robotcommand.has_wheel4=false;
-
-        /* This encodes the header for the field, based on the constant info
-         * from pb_field_t. */
-		if (!pb_encode_tag_for_field(stream, field))
-			return false;
-
-		/* This encodes the data for the field, based on our FileInfo structure. */
-        if (!pb_encode_submessage(stream, grSim_Robot_Command_fields, &robotcommand))
-            return false;
-
-	}
-    return true;
+bool decodifica_pacote(uint8_t *buffer, size_t size, grSim_Robot_Command *robotcommand){
+	pb_istream_t istream = pb_istream_from_buffer(buffer, size);
+	bool status=pb_decode(&istream, grSim_Robot_Command_fields, robotcommand);
+	return status;
 }
 
+uint8_t this_robot_id=0;
 
-bool robot_commands_decode_callback(pb_istream_t *stream, const pb_field_t *field, void **arg)
-{
-	grSim_Robot_Command robotcommand={};
+void set_speeds(float vel_x, float vel_y, float vel_theta){
 
-	if (!pb_decode(stream, grSim_Robot_Command_fields, &robotcommand))
-        return false;
-
-    return true;
 }
 
 int main(void){
-	uint8_t buffer[128];
-	size_t message_length;
-	bool status;
-	grSim_Commands message=grSim_Commands_init_zero;
-	pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-	message.isteamyellow=true;
-	message.timestamp=10.0f;
-	message.robot_commands.funcs.encode=&robot_commands_encode_callback;
-    status = pb_encode(&stream, grSim_Commands_fields, &message);
-    message_length = stream.bytes_written;
-
-
-	pb_istream_t istream = pb_istream_from_buffer(buffer, sizeof(buffer));
-	message.robot_commands.funcs.decode=&robot_commands_decode_callback;
-    status=pb_decode(&istream, grSim_Commands_fields, &message);
-
+	grSim_Robot_Command robotcommand;
+	uint8_t buffer[128]={8, 0, 21, 0, 0, 0, 0, 29, 0, 0, 0, 0, 37, 0, 0, 128, 63, 45, 0, 0, 0, 64, 53, 0, 0, 64, 64, 56, 0, 64, 0 , 0};
 	int i = 0;
 
-  /**
-  *  IMPORTANT NOTE!
-  *  The symbol VECT_TAB_SRAM needs to be defined when building the project
-  *  if code has been located to RAM and interrupts are used. 
-  *  Otherwise the interrupt table located in flash will be used.
-  *  See also the <system_*.c> file and how the SystemInit() function updates 
-  *  SCB->VTOR register.  
-  *  E.g.  SCB->VTOR = 0x20000000;  
-  */
+	grSim_Robot_Command gr=grSim_Robot_Command_init_zero;
 
-  /* TODO - Add your application code here */
+	if(decodifica_pacote(buffer, 31, &gr)  && gr.id==this_robot_id){
+		set_speeds(gr.velnormal, gr.veltangent, gr.velangular);
+	}
 
-  /* Initialize LEDs */
-  STM_EVAL_LEDInit(LED3);
-  STM_EVAL_LEDInit(LED4);
-  STM_EVAL_LEDInit(LED5);
-  STM_EVAL_LEDInit(LED6);
 
-  /* Turn on LEDs */
-  STM_EVAL_LEDOn(LED3);
-  STM_EVAL_LEDOn(LED4);
-  STM_EVAL_LEDOn(LED5);
-  STM_EVAL_LEDOn(LED6);
 
-  /* Infinite loop */
-  while (1)
-  {
-	i++;
-  }
+
+	/* Initialize LEDs */
+	STM_EVAL_LEDInit(LED3);
+	STM_EVAL_LEDInit(LED4);
+	STM_EVAL_LEDInit(LED5);
+	STM_EVAL_LEDInit(LED6);
+
+	/* Turn on LEDs */
+	STM_EVAL_LEDOn(LED3);
+	STM_EVAL_LEDOn(LED4);
+	STM_EVAL_LEDOn(LED5);
+	STM_EVAL_LEDOn(LED6);
+
+	/* Infinite loop */
+	while (1)
+	{
+		i++;
+	}
 }
 
 
@@ -141,8 +94,8 @@ int main(void){
  * Refer to stm32f4_discovery_audio_codec.h for more info.
  */
 extern "C" void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
-  /* TODO, implement your code here */
-  return;
+	/* TODO, implement your code here */
+	return;
 }
 
 /*
@@ -150,6 +103,6 @@ extern "C" void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t 
  * Refer to stm324xg_eval_audio_codec.h for more info.
  */
 extern "C" uint16_t EVAL_AUDIO_GetSampleCallBack(void){
-  /* TODO, implement your code here */
-  return -1;
+	/* TODO, implement your code here */
+	return -1;
 }
